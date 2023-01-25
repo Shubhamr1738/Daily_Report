142ce0f9b2bacdf4a6863163c56c32fa2d5d0f16
Senpai <shubhamrawal9922@gmail.com>
2023-01-26 01:54:16 +0530

    new data added

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,13 +1,12 @@
 const express = require("express")
 const router = express.Router();
-const { createUserData, getUserData,updateUserData, addlabour }= require("../controller/user-controller.js");
+const { createUserData, getUserData,updateUserData, addlabour,addLabourReports }= require("../controller/user-controller.js");
 
 
 router.get('/user',getUserData);
 router.post('/user',createUserData);
 router.post('/user/update',updateUserData);
 
-router.patch("/report/:site",addlabour); 
-  
-
+router.patch("/report/:site",addlabour);
+router.post("/report/:site",addLabourReports);
 module.exports = router;
